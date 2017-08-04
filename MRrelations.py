69372f2ffafccbe342158b1a1dc02d52d0a31c19
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-08-04 11:21:39 -0400

    added WM14 mass upper limit to MR relations

--- MRrelations.py
+++ MRrelations.py
@@ -19,18 +19,57 @@ def WM14(rps):
         Planetary masses in Earth masses corresponding to the planetary radii 
         given in `rps'.
     '''
+    rps = np.ascontiguousarray(rps)
     maxE, maxN = 1.5, 4.
-    
-    rps = np.array(rps)
-    assert not np.any(rps > maxN)
 
+    # Check
+    if np.any(rps >= maxN):
+	raise ValueError('WM14 mass-radius relation is only valid for ' + \
+			 'planets with radii < %i Earth radii.'%maxN)
+
+    # Compute mean mass for each planet radius
     mps = np.zeros(rps.size)
     for i in range(rps.size):
 
         if rps[i] < maxE:
-            mps[i] = .44 * rps[i]**3 + .614 * rps[i]**4
+            mps[i] = .441 * rps[i]**3 + .615 * rps[i]**4
 
         else:
             mps[i] = 2.69 * rps[i]**(.93)
+
+    return mps
+
+
+def WM14_upperlimit(rps):
+    '''
+    Compute the 1 sigma upper limit on the planetary mass from input planetary 
+    radii based on the mean mass-radius relation plus rms for small planets (radius 
+    <= 4 Earth radii) from Weiss & Marcy 2014 (Eqs. 1-3).
+
+    Parameters
+    ----------
+    `rps': float or array-like
+        Value(s) of the planetary radii that will be converted to a planetary
+        mass. Units of the input radii are assumed to be in Earth radii.
+
+    Returns
+    -------
+    `mps': numpy-array
+        Planetary masses in Earth masses corresponding to the planetary radii
+        given in `rps'.
+    '''
+    # Compute mean mass
+    rps = np.ascontiguousarray(rps)
+    mps = WM14(rps)
+
+    # Add 1 sigma upper limit to mean masses
+    maxE, rmsE, rmsN = 1.5, 2.7, 4.7
+    for i in range(mps.size):
+
+        if rps[i] < maxE:
+            mps[i] += rmsE
+
+        else:
+            mps[i] += rmsN
 
     return mps
